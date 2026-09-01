0925ddd224de67024962078088db5c9f40768486
Akuma_bot <nicollasbonitaao@gmail.com>
2026-09-01 01:14:48 -0300

    Create auth-login.js

---
+++ netlify/functions/auth-login.js
@@ -0,0 +1,13 @@
+exports.handler = async () => {
+  const params = new URLSearchParams({
+    client_id: process.env.DISCORD_CLIENT_ID,
+    redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    response_type: "code",
+    scope: "identify guilds",
+  });
+  return {
+    statusCode: 302,
+    headers: { Location: `https://discord.com/oauth2/authorize?${params.toString()}` },
+    body: "",
+  };
+};
